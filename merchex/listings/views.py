ce0bc3d41834777a4988bd5d24fe44f59ea7d9f8
coundia <papa.coundia@m2t.biz>
2021-12-03 16:56:18 +0000

    creer template OK

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+from  listings.models import Band
 # Create your views here.
 
+def hello(request):
+    bands = Band.objects.all()
+    return render(request,'listings/hello.html', {'bands' : bands} )
+
+def about(request):
+    return HttpResponse("<h1>About us </h1>")
+
+
+def contact(request):
+    return HttpResponse("<h1>Contact us </h1>")
+
+def help(request):
+    return HttpResponse("<h1>Contact us </h1>")
--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from  listings import views
 
 urlpatterns = [
+    path('', views.hello),
     path('admin/', admin.site.urls),
+    path('hello/', views.hello),
+    path('about-us/', views.about),
+    path('contact-us/', views.contact),
+    path('help/', views.help),
 ]
